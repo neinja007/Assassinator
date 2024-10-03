4f6035608efb92561fd97ff3ba6b9882457f6cd6
neinja007 <anton.siligan@gmail.com>
2024-10-03 14:16:56 +0200

    add basic logic

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,15 +1,19 @@
 'use client';
-
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const [count, setCount] = useState<number>(0);
 
+  useEffect(() => {}, []);
+
   return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ textAlign: 'center' }}>
+    <main className='flex h-screen items-center justify-center'>
+      <div className='text-center'>
         <h1>Count</h1>
         <p style={{ fontSize: '2rem' }}>{count}</p>
+        <button onClick={() => setCount(count + 1)} className='rounded-md bg-blue-500 p-2 text-white'>
+          Increment
+        </button>
       </div>
     </main>
   );
