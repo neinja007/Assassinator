691fc722c4ddd01f9e2fee3097e5c52e0b46f76c
neinja007 <anton.siligan@gmail.com>
2024-10-03 14:08:14 +0200

    lint to game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { metadata } from '@/data/metadata';
 import '@/app/globals.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return <h1 className='text-center text-4xl font-bold'>{metadata.title}</h1>;
+  return (
+    <div>
+      <h1 className='text-center text-4xl font-bold'>{metadata.title}</h1>
+
+      <Link href='/game'>Game</Link>
+    </div>
+  );
 }
